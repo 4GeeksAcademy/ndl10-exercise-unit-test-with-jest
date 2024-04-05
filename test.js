23239466dbe4ab0d161c99b920702eebc4b1ed86
ndl10 <159472389+ndl10@users.noreply.github.com>
2024-04-05 16:43:29 +0000

    correccion ejercicio

--- test.js
+++ test.js
@@ -19,7 +19,7 @@ test("One euro should be 1.07 dollars", function() {
     const expected = 3.5 * 1.07;
 
     // Hago mi comparación (la prueba)
-    expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+    expect(dollars).toBe(expected); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
 })
 
 test("One Euro should be 156.5 yens", function(){
@@ -29,7 +29,7 @@ test("One Euro should be 156.5 yens", function(){
 
     const expected = 1000 * 156.5;
 
-    expect(fromDollarToYen(1000)).toBe(156500);
+    expect(yens).toBe(expected);
 })
 
 test ("One Euro should be 0.87 pounds", function(){
@@ -38,5 +38,5 @@ test ("One Euro should be 0.87 pounds", function(){
     const pounds = fromYenToPound(100);
 
     const expected = 100 * 0.87;
-    expect(fromYenToPound(100)).toBe(87);
+    expect(pounds).toBe(expected);
 })
